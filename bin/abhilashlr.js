b46f0c303eae046d3a545dc18e8381331ff8630a
Abhilash L R <abhilashlr.789@gmail.com>
2020-02-28 17:45:35 +0530

    Bump

---
+++ bin/abhilashlr.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+console.warn("Hey there! Abhilash here...");
+console.warn("I work as a Lead Frontend Engineer at Freshworks...");
+console.warn("Feel free to reach out to me on Twitter at https://twitter.com/abhilashlr");
+
+const resume = [
+  {
+    time: "October 2011 - September 2014",
+    position: "UI Developer",
+    company: "CTS"
+  },
+  {
+    time: "October 2014 - February 2015",
+    position: "Frontend Developer",
+    company: "TCS"
+  },
+  {
+    time: "February 2015 - July 2018",
+    position: "Lead Frontend Engineer",
+    company: "Freshworks"
+  },
+  {
+    time: "August 2018 - January 2019",
+    position: "Frontend Engineer",
+    company: "Linkfire"
+  },
+  {
+    time: "February 2019 - Present",
+    position: "Lead Frontend Engineer",
+    company: "Freshworks"
+  }
+];
+
+console.warn("--------------------------");
+console.warn("Resume:");
+console.table(resume);
